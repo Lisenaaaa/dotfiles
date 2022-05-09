0a7e6bbf8b2c1f3cec2acbda9f573be12a15d1db
lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2022-05-08 17:14:08 -0700

    add kitty

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-paru -S bspwm sxhkd dunst picom polkit-kde-agent redshift xwallpaper deno playerctl pulsemixer eww-git nerd-fonts-fira-code
+paru -S bspwm sxhkd dunst picom polkit-kde-agent redshift xwallpaper deno playerctl pulsemixer eww-git nerd-fonts-fira-code kitty
+
+mkdir ~/.config
 
 mv ~/.config/bspwm ~/.config/bspwm-old
 ln -s ~/dotfiles/bspwm ~/.config/bspwm
 
 mv ~/.config/rofi ~/.config/rofi-old
 ln -s ~/dotfiles/rofi ~/.config/rofi
+
+mv ~/.config/kitty ~/.config/kitty-old
+ln -s ~/dotfiles/kitty ~/.config/kitty
